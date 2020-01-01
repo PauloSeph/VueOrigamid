724765a16a537a4798d0790634b2d7fcd7f54ba9
Seph <paulo.seph@hotmail.com>
2020-01-01 19:35:52 -0300

    Events + VueClI, estrutura

---
+++ exercicios/app-vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{mensagem}}
+    <HeaderPrincipal/>
+  </div>
+</template>
+
+<script>
+
+export default {
+ 
+  data() {
+    return {
+      mensagem: "Está é uma mensagem"
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
